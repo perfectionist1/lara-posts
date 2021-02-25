7878e8667a23322aaad50f6c34907497aa9f9d5c
Md. Mostafizur Rahman <dev.mostafizur@gmail.com>
2021-02-25 20:10:01 +0600

    Initial stage of Posts project, creating in Laravel

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+</head>
+<body class="bg-gray-200">
+    <nav class="py-5 bg-white flex justify-between mb-3">
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3">Home</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Dashboard</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Posts</a>
+            </li>
+        </ul>
+
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3">Mostafiz</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Login</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Register</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Logout</a>
+            </li>
+        </ul>
+    </nav>
+
+    @yield('content')
+    
+</body>
+</html>
